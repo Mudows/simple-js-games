34d5455042e5109a5da9e38109c34df892336640
Diego C. Cezar <dmcezar@gmail.com>
2022-03-10 00:46:31 -0300

    Whac-a-Mole base complete. Need refining of the logic.

--- whacamole.js
+++ whacamole.js
@@ -4,14 +4,46 @@ const timeLeft = document.querySelector("#whac-timer");
 const whacScore = document.querySelector("#whac-score");
 
 let result = 0;
+let hitPosition;
+let whacTimer = 60;
 
 function randomSquare() {
   whacSquares.forEach((square) => {
     square.classList.remove("mole");
   });
 
-  let randomPosition = whacSquares[Math.floor(Math.random() * 9)];
+  let randomSquare = whacSquares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id;
 }
 
-randomSquare();
+whacSquares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result += 1;
+            console.log(result);
+            whacScore.textContent = result;
+            hitPosition = null;
+        }
+    })
+})
+
+function moveMole() {
+    let timerId = null;
+    timerId = setInterval(randomSquare, 1000);
+}
+
+moveMole();
+
+function countDown() {
+    whacTimer--;
+    timeLeft.textContent = whacTimer;
+
+    if(whacTimer == 0) {
+        clearInterval(countDownTimerId);
+        clearInterval(timerId);
+    }
+}
+
+let countDownTimerId =  setInterval(countDown, 1000);
